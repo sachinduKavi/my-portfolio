5e9156e8cd38cfa3b3fb355b7e0dfe2d56692247
sachinduKavi <cst21017@std.uwu.ac.lk>
2024-10-07 18:25:23 +0530

    projects

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,15 +1,67 @@
-import React from 'react'
+import React, {useEffect} from 'react'
+import { user } from '../data/user'
+import VanillaTilt from 'vanilla-tilt'
+import {motion} from 'framer-motion'
 
 
 import '../styles/projects.css'
 
 export default function Projects() {
 
+  const projects = user.projects
 
+
+  useEffect(() =>{
+    const element = document.querySelectorAll(".project-card");
+    if(element) {
+      console.log(element)
+      VanillaTilt.init(element);
+
+      return(() => {
+        element.vanillaTilt?.destroy();
+
+        // Reset instance
+        element.vanillaTilt?.reset();
+      })
+    }
+    
+  })
 
   return (
-    <div className='projects'>
+    <>
+      <h2 className="title">Projects</h2>
+      <div className='projects-container'>
+      
+      {
+        projects.map((element, index) => {
+          return (
+            <div className="project-card"
+            key={index}
+            style={{
+              background: `url('${element.image}')`,
+              backgroundPosition: 'left',
+              backgroundSize: 'cover'
+              }}>
 
+                <div className="project-name">
+                  <h2>{element.name}</h2>
+                </div>
+
+
+                <motion.div className="description">
+                  <p>{element.description}</p>
+                </motion.div>
+
+            </div>
+          )
+        })
+      }
+      
+
+    
     </div>
+    
+    </>
+    
   )
 }
